c49e152464bdc7de2ee657affd02b501a6de8eb5
Bibhav Yadav <managementlucky07@gmail.com>
2024-06-09 17:45:17 +0530

    feat: ant design added login page design

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'antd/dist/reset.css';
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { ConfigProvider } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token:{
+          colorPrimary: '#F65F42',
+          colorLink:'#F65F42'
+        }
+      }}
+    >
     <RouterProvider router={router}/>
+    </ConfigProvider>
   </React.StrictMode>,
 )
