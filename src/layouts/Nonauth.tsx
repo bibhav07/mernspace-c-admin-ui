05f7bd047d025e7148d39dc8e9f93eae570d7351
Bibhav Yadav <managementlucky07@gmail.com>
2024-06-16 10:00:24 +0530

    feat: added nested routes

---
+++ src/layouts/Nonauth.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+const Nonauth = () => {
+  return (
+    <div>Nonauth
+            <Outlet/>
+
+    </div>
+  )
+}
+
+export default Nonauth
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/login/login";
+import Dashboard from "./layouts/Dashboard";
+import Nonauth from "./layouts/Nonauth";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <HomePage />
+        element: <Dashboard />,
+        children : [
+            {
+                path: '',
+                element: <HomePage />
+            }
+        ]
     },
     {
-        path: '/auth/login',
-        element: <LoginPage />
+        path: '/auth/',
+        element: <Nonauth />,
+        children : [
+            {
+                path: 'login',
+                element: <LoginPage />
+            }
+        ]
     }
 ])
